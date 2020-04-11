275b7ff963774f46276a08d1743eb56d6a48b6da
ItsSyfe <22247810+ItsSyfe@users.noreply.github.com>
2020-04-11 15:23:37 +0100

    Finished Plugin.

---
+++ src/main/java/me/syfe/swashbuckleenchants/SwashbuckleEnchants.java
@@ -0,0 +1,104 @@
+package me.syfe.swashbuckleenchants;
+
+import me.syfe.swashbuckleenchants.customenchants.ItemMagnetEnchantment;
+import org.bukkit.*;
+import org.bukkit.NamespacedKey;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public final class SwashbuckleEnchants extends JavaPlugin implements Listener {
+
+    private static SwashbuckleEnchants plugin;
+    public static ItemMagnetEnchantment itemMagnetEnchantment;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        plugin = this;
+        itemMagnetEnchantment = new ItemMagnetEnchantment("item_magnet");
+
+        registerEnchantment(itemMagnetEnchantment);
+
+        this.getServer().getPluginManager().registerEvents(itemMagnetEnchantment, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        try {
+            Field keyField = Enchantment.class.getDeclaredField("byKey");
+
+            keyField.setAccessible(true);
+            @SuppressWarnings("unchecked")
+            HashMap<NamespacedKey, Enchantment> byKey = (HashMap<NamespacedKey, Enchantment>) keyField.get(null);
+
+            if(byKey.containsKey(itemMagnetEnchantment.getKey())) {
+                byKey.remove(itemMagnetEnchantment.getKey());
+            }
+            Field nameField = Enchantment.class.getDeclaredField("byName");
+
+            nameField.setAccessible(true);
+            @SuppressWarnings("unchecked")
+            HashMap<String, Enchantment> byName = (HashMap<String, Enchantment>) nameField.get(null);
+
+            if(byName.containsKey(itemMagnetEnchantment.getName())) {
+                byName.remove(itemMagnetEnchantment.getName());
+            }
+        } catch (Exception ignored) { }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if(command.getName().equals("itmgive")){
+            if(sender.hasPermission("SwashbuckleEnchants.itemmagnet")){
+                Player player = (Player) sender;
+                if (player instanceof Player) {
+                    player.getInventory().getItemInMainHand().addEnchantment(SwashbuckleEnchants.itemMagnetEnchantment, 1);
+                    ItemMeta meta = player.getInventory().getItemInMainHand().getItemMeta();
+                    ArrayList<String> lore = new ArrayList<>();
+                    lore.add(ChatColor.LIGHT_PURPLE + "Item Magnet I");
+                    meta.setLore(lore);
+                    player.getInventory().getItemInMainHand().setItemMeta(meta);
+                    sender.sendMessage(ChatColor.GREEN + "[SwashbuckleEnchants] Successfully applied Item Magnet to item in hand!");
+                    return true;
+                }
+            } else {
+                sender.sendMessage(ChatColor.RED + "[SwashbuckleEnchants] You don't have permission to do that!");
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static SwashbuckleEnchants getPlugin(){
+        return plugin;
+    }
+
+    public static void registerEnchantment(Enchantment enchantment) {
+        boolean registered = true;
+        try {
+            Field f = Enchantment.class.getDeclaredField("acceptingNew");
+            f.setAccessible(true);
+            f.set(null, true);
+            Enchantment.registerEnchantment(enchantment);
+        } catch (Exception e) {
+            registered = false;
+            e.printStackTrace();
+        }
+        if(registered){
+            // It's been registered!
+        }
+    }
+
+}
---
+++ src/main/java/me/syfe/swashbuckleenchants/customenchants/ItemMagnetEnchantment.java
@@ -0,0 +1,87 @@
+package me.syfe.swashbuckleenchants.customenchants;
+
+import me.syfe.swashbuckleenchants.SwashbuckleEnchants;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Collection;
+
+public class ItemMagnetEnchantment extends Enchantment implements Listener {
+
+    public ItemMagnetEnchantment(String namespace) {
+        super(new NamespacedKey(SwashbuckleEnchants.getPlugin(), namespace));
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e){
+        if (!e.isDropItems()) return;
+
+        Player player = (Player) e.getPlayer();
+
+        Collection<ItemStack> drops = e.getBlock().getDrops(player.getInventory().getItemInMainHand());
+        ItemStack[] stack = drops.toArray(new ItemStack[drops.size()]);
+
+        if(player.getEquipment().getItemInMainHand().getEnchantments().containsKey(Enchantment.getByKey(SwashbuckleEnchants.itemMagnetEnchantment.getKey()))){
+            if(isInventoryFull(player) == false){
+                e.getPlayer().getInventory().addItem(drops.toArray(new ItemStack[0]));
+            } else {
+                player.getWorld().dropItem(player.getLocation().add(0, .5, 0), stack[0]);
+            }
+            e.getBlock().setType(Material.AIR);
+            e.setCancelled(true);
+        }
+    }
+
+    public boolean isInventoryFull(Player player)
+    {
+        return player.getInventory().firstEmpty() == -1;
+    }
+
+    @Override
+    public String getName() {
+        return "Item Magnet";
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 1;
+    }
+
+    @Override
+    public int getStartLevel() {
+        return 1;
+    }
+
+    @Override
+    public EnchantmentTarget getItemTarget() {
+        return EnchantmentTarget.TOOL;
+    }
+
+    @Override
+    public boolean isTreasure() {
+        return false;
+    }
+
+    @Override
+    public boolean isCursed() {
+        return false;
+    }
+
+    @Override
+    public boolean conflictsWith(Enchantment other) {
+        return false;
+    }
+
+    @Override
+    public boolean canEnchantItem(ItemStack item) {
+        return true;
+    }
+}
